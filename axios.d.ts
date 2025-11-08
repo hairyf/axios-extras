fed8b7872c0cb38e65a8fe4216377c522329afb5
hairyf <wwu710632@gmail.com>
2025-11-08 15:58:01 +0800

    feat: add TypeScript declaration file for axios module

---
+++ axios.d.ts
@@ -0,0 +1,6 @@
+/// <reference types="axios" />
+
+declare module 'axios' {
+  // This file ensures TypeScript can find the axios module during build
+  // The actual type augmentations are in individual feature files
+}
